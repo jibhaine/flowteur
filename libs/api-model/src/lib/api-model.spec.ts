97a3a5abfa5e16e9c086d475973f6114e2c4ade8
Benjamin Legrand <jibhaine@free.fr>
2020-04-03 20:48:10 +0200

    feat(api-model): add api-model module

---
+++ libs/api-model/src/lib/api-model.spec.ts
@@ -0,0 +1,7 @@
+import { apiModel } from './api-model';
+
+describe('apiModel', () => {
+  it('should work', () => {
+    expect(apiModel()).toEqual('api-model');
+  });
+});
---
+++ libs/api-model/src/lib/api-model.ts
@@ -0,0 +1,3 @@
+export function apiModel(): string {
+  return 'api-model';
+}
