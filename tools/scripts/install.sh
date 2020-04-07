65ed4c5e35347283fe97cd293615997deb720d52
Benjamin Legrand <jibhaine@free.fr>
2020-04-07 07:15:56 +0200

    feat(docker): init docker-compose and Dockerfiles

---
+++ tools/scripts/install.sh
@@ -0,0 +1,6 @@
+echo '127.0.0.1 minio-local.flowteur.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 couch-local.flowteur.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 serve-local.flowteur.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 rendr-local.flowteur.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 codec-local.flowteur.com' | sudo tee -a /etc/hosts
+echo '127.0.0.1 media-local.flowteur.com' | sudo tee -a /etc/hosts
